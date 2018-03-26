9866cd567fa01b23093fb04440e7499b8d5186a1
Danilo Assis <daniloassis.ti@gmail.com>
2018-03-26 16:52:16 -0300

    Creating html for display pokemon

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -29,10 +29,30 @@ angular.module('myApp.view1', ['ngRoute'])
       $.ajax({
         url: "https://pokeapi.co/api/v2/pokemon/" + pokemon + "/",
         type: 'GET',
-        success: function (resp) {
-          if (resp) {
+        success: function (data) {
+          if (data) {
             hideLoading();
-            console.log(resp);
+            $('.error').css('display', 'none');
+            console.log(data);
+
+            vm.name = data.name;
+            vm.number = data.national_id;
+            vm.type1 = data.types[0].name;
+
+            if(data.types.length == 2){
+              vm.type2 = data.types[1].name;
+            } else vm.type2 = null;
+
+            vm.descriptionURI = "http://pokeapi.co" + data.description[0].resource_uri;
+
+            $.ajax({
+              url: vm.descriptionURI,
+              type: 'GET',
+              success: function (data2) {
+                console.log(data2);
+              }
+            })
+
           }
         },
         error: function (e) {
