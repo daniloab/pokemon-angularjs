5303899d5fd267a3cadfa1bc7e87ccf6554eaa55
Danilo Assis <daniloassis.ti@gmail.com>
2018-03-26 15:54:28 -0300

    dev all app

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -2,13 +2,43 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/view1', {
+      templateUrl: 'view1/view1.html',
+      controller: 'View1Ctrl as vm'
+    });
+  }])
 
-.controller('View1Ctrl', [function() {
+  .controller('View1Ctrl', [function ($scope) {
 
-}]);
+    var vm = this;
+
+    vm.searchPokemon = searchPokemon;
+
+    function showLoading() {
+      $('.loading').fadeIn(500);
+      $('.loading').css('opacity', '1');
+    }
+
+    function hideLoading() {
+      $('.loading').fadeOut(500);
+    }
+
+    function searchPokemon(pokemon) {
+      showLoading();
+      $.ajax({
+        url: "https://pokeapi.co/api/v2/pokemon/" + pokemon + "/",
+        type: 'GET',
+        success: function (resp) {
+          if (resp) {
+            hideLoading();
+            console.log(resp);
+          }
+        },
+        error: function (e) {
+          hideLoading();
+          $('.error').css('display', 'block');
+        }
+      })
+    }
+  }]);
